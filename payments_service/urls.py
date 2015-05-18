ad1050395740f67197c3f02f8ecd551c9a04cb0e
Stuart Colville <scolville@mozilla.com>
2015-05-18 13:00:27 +0100

    Move service urls under /api/ (fixes #15)

--- payments_service/urls.py
+++ payments_service/urls.py
@@ -1,13 +1,17 @@
 from django.conf.urls import include, url
 
 
-urlpatterns = [
+api_patterns = [
     url(r'^auth/',
         include('payments_service.auth.urls', namespace='auth')),
     url(r'^braintree/',
         include('payments_service.braintree.urls', namespace='braintree')),
-    url(r'',
+    url(r'^$',
         include('payments_service.status.urls', namespace='status')),
+]
+
+urlpatterns = [
+    url(r'^api/', include(api_patterns))
 ]
 
 handler500 = 'payments_service.base.views.error_500'
