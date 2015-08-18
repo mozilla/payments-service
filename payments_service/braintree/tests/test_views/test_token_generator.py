38ff6bb14052856100687ddc094e23c4b1cd2388
Kumar McMillan <kumar.mcmillan@gmail.com>
2015-08-18 11:06:13 -0500

    Generate braintree token anonymously for donations

--- payments_service/braintree/tests/test_views/test_token_generator.py
+++ payments_service/braintree/tests/test_views/test_token_generator.py
@@ -2,25 +2,29 @@ from django.core.urlresolvers import reverse
 
 from nose.tools import eq_
 
-from payments_service.base.tests import AuthenticatedTestCase
+from payments_service.base.tests import TestCase
 
 
-class TestTokenGenerator(AuthenticatedTestCase):
+class TestTokenGenerator(TestCase):
 
     def setUp(self):
         super(TestTokenGenerator, self).setUp()
         self.url = reverse('braintree:token.generate')
+        self.braintree_token = 'some-token'
+        self.solitude.braintree.token.generate.post.return_value = {
+            'token': self.braintree_token,
+        }
 
     def post(self):
         res = self.client.post(reverse('braintree:token.generate'))
         return self.json(res)
 
-    def test_api_connection(self):
-        # This is just a simple test to make sure the endpoint is
-        # connected to Solitude.
-        self.solitude.braintree.token.generate.post.return_value = {
-            'token': 'some-token',
-        }
+    def test_get_braintree_token(self):
         res, data = self.post()
         eq_(res.status_code, 200, res)
-        eq_(data['token'], 'some-token')
+        eq_(data['token'], self.braintree_token)
+
+    def test_get_csrf_token(self):
+        res, data = self.post()
+        eq_(res.status_code, 200, res)
+        assert 'csrf_token' in data, data
--- payments_service/braintree/views/token_generator.py
+++ payments_service/braintree/views/token_generator.py
@@ -1,9 +1,29 @@
-from payments_service.solitude import SolitudeBodyguard
+from django.middleware import csrf
+
+from rest_framework.response import Response
+
+from payments_service import solitude
+from payments_service.base.views import UnprotectedAPIView
 
 
-class TokenGenerator(SolitudeBodyguard):
+class TokenGenerator(UnprotectedAPIView):
     """
     Generate a client token to begin processing payments.
     """
-    methods = ['post']
-    resource = 'braintree.token.generate'
+
+    def post(self, request):
+        bt_data = solitude.api().braintree.token.generate.post()
+
+        # Generate a new token for added security.
+        csrf.rotate_token(request)
+
+        # This view returns a CSRF token to the client so that it can
+        # initialize API usage for anonymous payments (e.g. donations).
+        # For added security, we could restrict the scope of that CSRF
+        # tokens only to views relating to products that support
+        # anonymous payments.
+
+        return Response({
+            'token': bt_data['token'],
+            'csrf_token': csrf.get_token(request),
+        })
